1409cd5bc8ae6f489802e88f370b00731780cfa6
kappa_maintainer <gkappa@outlands.top>
2024-03-28 22:09:37 +0800

    Add warning on Forge

--- src/main/java/com/cleanroommc/scalar/ScalarLoadingPlugin.java
+++ src/main/java/com/cleanroommc/scalar/ScalarLoadingPlugin.java
@@ -1,14 +1,30 @@
 package com.cleanroommc.scalar;
 
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.FMLModContainer;
+import net.minecraftforge.fml.common.ILanguageAdapter;
+import net.minecraftforge.fml.relauncher.FMLSecurityManager;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.annotation.Nullable;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.util.Map;
 
 public class ScalarLoadingPlugin implements IFMLLoadingPlugin {
     public static Logger LOGGER = LogManager.getLogger("Scalar");
+
+    ScalarLoadingPlugin() {
+        try {
+            MethodHandles.lookup().findStatic(FMLModContainer.class, "registerLanguageAdapter", MethodType.methodType(Void.class, String.class, ILanguageAdapter.class)).invoke("scala", new ScalaLanguageAdapter());
+        } catch (Throwable ex) {
+            LOGGER.fatal("Not running on Cleanroom!");
+            FMLCommonHandler.instance().exitJava(1, true);
+        }
+    }
     @Override
     public String[] getASMTransformerClass() {
         return new String[0];
